823e7ed03cd7e2a925e93c214ff404210970d746
archuna <archunaramakrishnan@gmail.com>
2023-08-19 12:31:29 +0530

    first commit

---
+++ JAVASCRIPT-TRAINING/variables-arthmetic-operator.js
@@ -0,0 +1,9 @@
+// let x = 500;
+// let y= 30;
+// console.log(x+5);
+// let CUSTOM = ""
+// let a=10 , b=20;
+// let carName;
+// console.log(carName)
+// carName = 'Audi'
+// // console.log(carName)
